9c75b8e0e096f895ffcc4f2bbe1860fcf860b166
Daniel <Schattenpalkia@googlemail.com>
2024-10-09 10:00:46 +0200

    quiz day 1

--- src/question.js
+++ src/question.js
@@ -1,7 +1,18 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
 
-    // 2. shuffleChoices()
+    shuffleChoices() {
+        /* Randomize array in-place using Durstenfeld shuffle algorithm */
+        for (var i = this.choices.length - 1; i >= 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = this.choices[i];
+            this.choices[i] = this.choices[j];
+            this.choices[j] = temp;
+        }
+    }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,37 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
 
-    // 4. shuffleQuestions()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
 
-    // 5. checkAnswer(answer)
+    shuffleQuestions() {
+        /* Randomize array in-place using Durstenfeld shuffle algorithm */
+        for (var i = this.questions.length - 1; i >= 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = temp;
+        }
+    }
 
-    // 6. hasEnded()
+    checkAnswer(answer) {
+        if (answer == this.questions[this.currentQuestionIndex].answer) {
+            this.correctAnswers++;
+        }
+    }
+
+    hasEnded() {
+        return !(this.currentQuestionIndex < this.questions.length);
+    }
 }
